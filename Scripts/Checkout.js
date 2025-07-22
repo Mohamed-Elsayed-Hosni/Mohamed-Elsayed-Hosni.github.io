7b425b153874505218222c6732ffd847563784fb
Mohamed ElSayed Hosni <mohamed.mohamed057@eng-st.cu.edu.eg>
2025-07-22 18:46:18 +0300

    Javascript
    help

---
+++ Scripts/Checkout.js
@@ -0,0 +1,29 @@
+const FirstName = document.querySelector("#First");
+const LastName = document.querySelector("#Last");
+const Email = document.querySelector("#Email");
+const Address = document.querySelector("#Address");
+const Phone = document.querySelector("#Phone");
+const msg =document.querySelector(".ERROROFF")
+const Form= document.querySelector("#Checkout");
+
+Form.addEventListener('submit',onsubmit);
+
+function onsubmit(e){
+e.preventDefault();
+if(FirstName.value =='' || LastName.value =='' || Email.value =='' || Phone.value =='' || Address.value =='' ){
+    msg.classList.add('ERRORON');
+    msg.classList.remove('ERROROFF');
+    msg.innerHTML="Please Enter all Fields to Validate your Purchase";
+
+    setTimeout(()=>{
+    msg.classList.remove('ERRORON');
+    msg.classList.add('ERROROFF');
+    msg.innerHTML="";}
+     ,3000)
+    }
+else {
+    msg.innerHTML=`Thank you, ${FirstName.value} for your purchase of The Art of War`;
+    setTimeout(() => {msg.innerHTML=''},5000); 
+}
+
+}
---
+++ Scripts/index.js
@@ -0,0 +1,29 @@
+const Username = document.querySelector("#Username");
+const Password = document.querySelector("#Password");
+const msg =document.querySelector(".ERROROFF")
+const Form= document.querySelector("#Sign");
+
+Form.addEventListener('submit',onsubmit);
+
+function onsubmit(e){
+e.preventDefault();
+if(Username.value =='' || Password.value ==''){
+    
+    msg.classList.add('ERRORON');
+    msg.classList.remove('ERROROFF');
+    msg.innerHTML="Please Enter all Fields to Sign in or Enter as Guest";
+
+    setTimeout(()=>{
+    msg.classList.remove('ERRORON');
+    msg.classList.add('ERROROFF');
+    msg.innerHTML="";}
+     ,3000)
+    }
+else {
+    msg.innerHTML=`Welcome back, ${Username.value}`;
+    Username.disabled=true;
+    Password.disabled=true;
+    setTimeout(() => {window.location.assign("Homepage.html")},2000); 
+}
+
+}
